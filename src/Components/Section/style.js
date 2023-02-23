79f8c445082fd1a0d7eceeb0b7f0b108c3f56b0c
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2023-02-23 16:24:49 -0300

    Correct rank stars

--- src/Components/Section/style.js
+++ src/Components/Section/style.js
@@ -3,12 +3,16 @@ import styled from 'styled-components'
 export const Container = styled.section`
   margin: 24px 0;
 `
-export const Title = styled.span`
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 19px;
+
+  margin-bottom: 24px;
+
   > h2 {
     color: ${({ theme }) => theme.COLORS.OFFWHITE};
     font-size: 36px;
     font-weight: 500;
-
-    margin-bottom: 24px;
   }
 `
